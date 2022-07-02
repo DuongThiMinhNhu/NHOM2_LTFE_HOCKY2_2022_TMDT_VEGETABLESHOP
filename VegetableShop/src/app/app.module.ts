10d217e0136262283fff21d97fbf1346f4f42752
DuongThiMinhNhu <76153599+DuongThiMinhNhu@users.noreply.github.com>
2022-07-02 12:37:02 +0700

    setuplibry
    nhu set up thu vien va phan comment

--- VegetableShop/src/app/app.module.ts
+++ VegetableShop/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './component/cart/cart.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    FooterComponent,
+    ContactComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
