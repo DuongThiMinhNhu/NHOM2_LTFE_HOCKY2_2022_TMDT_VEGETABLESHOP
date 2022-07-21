48b2c85ca7eb0c68d23756b1efad818441de597c
KyAnhNguyen <19130011@st.hcmuaf.edu.vn>
2022-07-21 11:13:54 +0700

    add menu view

--- VegetableShop/src/app/component/menu/menu.component.ts
+++ VegetableShop/src/app/component/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-menu',
@@ -6,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-
-  constructor() { }
-
+  private _jsonProduct = 'assets/data/products.json';
+  products: any = [];
+  constructor(private http: HttpClient) {
+    this.getJSON().subscribe(data => {
+      this.products = data;
+    })
+  }
+  public getJSON(): Observable<any> {
+    return this.http.get(this._jsonProduct);
+  }
   ngOnInit(): void {
   }
 
