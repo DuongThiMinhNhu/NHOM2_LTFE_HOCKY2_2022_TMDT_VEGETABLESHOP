0cbbfe44c3c77a916121583c0a3587aabd110842
DuongThiMinhNhu <76153599+DuongThiMinhNhu@users.noreply.github.com>
2022-07-28 08:12:38 +0700

    nhu_page_home
    nhu_page_home

---
+++ VegetableShop/src/app/component/home/endow/endow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-endow',
+  templateUrl: './endow.component.html',
+  styleUrls: ['./endow.component.scss']
+})
+export class EndowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
