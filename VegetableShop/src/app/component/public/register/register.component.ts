6fa904313d795ef582e39fda71fc9ed40d5b5dbf
KyAnhNguyen <19130011@st.hcmuaf.edu.vn>
2022-08-05 11:11:19 +0700

    I do validation register form
    100%

--- VegetableShop/src/app/component/public/register/register.component.ts
+++ VegetableShop/src/app/component/public/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -6,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('registerForm')
+  registerForm: NgForm;
+  constructor(private  router: Router) { }
+  onSubmit (){
+    if(!this.registerForm.value){
+      console.log('invalid data');
+      return;
+    }
+    console.log(this.registerForm.value);
+  }
 
   ngOnInit(): void {
   }
