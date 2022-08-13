0d98ba3382e5a2311fb7412ee5f7f5da326c61c5
DuongThiMinhNhu <76153599+DuongThiMinhNhu@users.noreply.github.com>
2022-08-14 01:30:57 +0700

    do_comment
    do_comment

--- VegetableShop/src/app/component/authenticated/detail-product/detail-product.component.ts
+++ VegetableShop/src/app/component/authenticated/detail-product/detail-product.component.ts
@@ -19,4 +19,17 @@ export class DetailProductComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  myClickFunctionResult(envent){
+    const al1 = document.querySelector('.answer')
+    if (al1.className == "answer") {
+      document.querySelector('.answer').classList.add('active');
+      console.log("Mở");
+      document.querySelector('.result2').classList.add('result1');
+    } else {
+      console.log("Đóng");
+      document.querySelector('.answer').classList.remove('active');
+      document.querySelector('.result2').classList.remove('result1');
+    }
+
+  }
 }
