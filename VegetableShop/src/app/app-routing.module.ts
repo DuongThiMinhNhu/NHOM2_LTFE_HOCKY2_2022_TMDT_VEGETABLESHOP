5e54899f5e13c713ef77680b7dd2d5c52d6df81c
thanhnguyenle <19130206@st.hcmuaf.edu.vn>
2022-08-19 22:27:14 +0700

    update blogdetail and blog card component

--- VegetableShop/src/app/app-routing.module.ts
+++ VegetableShop/src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ import {ProfileComponent} from "./component/authenticated/profile/profile.compon
 import {DetailProductComponent} from "./component/authenticated/detail-product/detail-product.component";
 import {AboutComponent} from "./component/authenticated/about/about.component";
 import {SettingComponent} from "./component/authenticated/setting/setting.component";
-import {DetailBlogComponent} from "./component/authenticated/blog/detail-blog/detail-blog.component";
+import {DetailBlogComponent} from "./component/authenticated/blog/detail-post/detail-blog.component";
 import {CheckOutComponent} from "./component/authenticated/check-out/check-out.component";
 
 const routes: Routes = [
@@ -26,7 +26,7 @@ const routes: Routes = [
         ],},
     {path:'', component:AuthenticatedComponent,
     children: [
-        {path: 'blog' , component: BlogComponent},
+        {path: 'blog', component: BlogComponent},
         {path: 'cart' , component: CartComponent},
         {path: 'contact' , component: ContactComponent},
         {path: 'about' , component: AboutComponent},
@@ -35,7 +35,6 @@ const routes: Routes = [
         {path: 'menu' , component: MenuComponent},
         {path: 'profile' , component: ProfileComponent},
         {path: 'setting' , component: SettingComponent},
-        {path: 'detail-blog/:id' , component: DetailBlogComponent},
         {path: 'check-out' , component: CheckOutComponent},
         {path: '' ,pathMatch:"full", redirectTo:"home"},
     ],},
