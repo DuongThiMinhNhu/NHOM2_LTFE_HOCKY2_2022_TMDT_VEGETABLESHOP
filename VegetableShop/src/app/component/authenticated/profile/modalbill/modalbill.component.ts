8fcd5d1fa12e9201f9e872d2af79069efb2a81fa
DuongThiMinhNhu <76153599+DuongThiMinhNhu@users.noreply.github.com>
2022-08-23 19:32:34 +0700

    history_bill
    history_bill

---
+++ VegetableShop/src/app/component/authenticated/profile/modalbill/modalbill.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modalbill',
+  templateUrl: './modalbill.component.html',
+  styleUrls: ['./modalbill.component.scss']
+})
+export class ModalbillComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
