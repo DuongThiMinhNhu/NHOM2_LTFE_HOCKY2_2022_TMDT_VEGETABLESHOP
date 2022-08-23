8807e94c0853bff8d15d4b07bbbd166b6b4f2f96
KyAnhNguyen <19130011@st.hcmuaf.edu.vn>
2022-08-23 21:25:40 +0700

    fix cart view, check out view

--- VegetableShop/src/app/component/authenticated/check-out/cart-total/cart-total.component.ts
+++ VegetableShop/src/app/component/authenticated/check-out/cart-total/cart-total.component.ts
@@ -2,6 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {Cart} from "../../../../models/cart";
 import {SessionKey} from "../../../../../assets/resources/sessionkey";
 import {Account} from "../../../../models/account";
+import {CartService} from "../../../../services/cart/cart.service";
+import {TaxService} from "../../../../services/tax/tax.service";
+import {Observable} from "rxjs";
+import {Tax} from "../../../../models/tax";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-cart-total',
@@ -9,14 +15,49 @@ import {Account} from "../../../../models/account";
     styleUrls: ['./cart-total.component.scss']
 })
 export class CartTotalComponent implements OnInit {
-    cart: Cart;
-
-    constructor() {
-        this.cart = JSON.parse(sessionStorage.getItem(SessionKey.CART));
-        console.log(this.cart)
+    cartService: CartService;
+    taxService:TaxService;
+    tax:Observable<Tax>;
+    constructor(private http:HttpClient,private router:Router) {
+        this.cartService = CartService.getInstance();
+        this.cartService = CartService.getInstance();
+        this.taxService = TaxService.getInstance(http);
     }
 
     ngOnInit(): void {
+        this.getShippingByWeight();
     }
+    getShippingByWeight(){
+        let weight = this.cartService.weightCart();
+        if(weight<1000) {
+            this.taxService.doGetById("1000").then(
+                re=>{
+                    if(re!=null)
+                        this.tax = re;
+                }
+            )
+        }else if(weight<10000){
+            this.taxService.doGetById("10000").then(
+                re=>{
+                    if(re!=null)
+                        this.tax = re;
+                }
+            )
+        }else if(weight<20000){
+            this.taxService.doGetById("20000").then(
+                re=>{
+                    if(re!=null)
+                        this.tax = re;
+                }
+            )
+        }else if(weight>=20000){
+            this.taxService.doGetById("30000").then(
+                re=>{
+                    if(re!=null)
+                        this.tax = re;
+                }
+            )
+        }
 
+    }
 }
